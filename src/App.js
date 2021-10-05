d5f7d9f0573cad9464ab73c09856cef2baf52a55
patryckw7 <patrykw3@o2.pl>
2021-10-05 20:25:57 +0200

    optimization of styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import './App.css';
+import './styles/main.scss';
 import UserList from './components/UserList';
 import Home from './components/Home';
 import Navbar from './components/Navbar/Navbar';
